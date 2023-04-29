19753be6347b7ef7c8c06b973da5a5968dabb70b
balarobertvalentin <balarobertvalentin@gmail.com>
2023-04-29 04:49:19 +0300

    Adding a Helper Function that retrieves the Production Version

---
+++ HelperFunctions/GetProductionVersion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class VersionHelper
+{
+    public static string GetProductionVersion(string[] versions)
+    {
+        Version highestVersion = new Version(0, 0, 0);
+        string highestVersionString = "";
+
+        foreach (string versionString in versions)
+        {
+            string[] parts = versionString.Split('-');
+            Version version = new Version(parts[0]);
+            if (version.CompareTo(highestVersion) > 0)
+            {
+                highestVersion = version;
+                highestVersionString = parts.Length > 1 ? $"{parts[0]}-{parts[1]}" : parts[0];
+            }
+        }
+
+        return highestVersionString;
+    }
+}
